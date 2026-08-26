6523be205349be4059aa689d60fda780692fa07d
CosmicJA96 <23452@burnside.school.nz>
2026-08-27 10:03:14 +1200

    clean python code

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask,g, render_template
+from flask import Flask, g, render_template
 import sqlite3
 DATABASE = 'database.db'
 app = Flask(__name__)
+
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -9,11 +10,13 @@ def get_db():
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
+
 def query_db(query, args=(), one=False):
     cur = get_db().execute(query, args)
     rv = cur.fetchall()
     cur.close()
     return (rv[0] if rv else None) if one else rv
+
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -21,9 +24,16 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
-############################# Routes ###################################################################
 
-#Home
+@app.errorhandler(404)
+def not_found(error):
+    return render_template("404.html"), 404
+
+
+# Routes ##########################################
+
+# Home
+
 
 @app.route("/")
 def home():
@@ -31,30 +41,34 @@ def home():
         SELECT ID, Name, ImageURL FROM Planets;""")
     return render_template("home.html", planets=planets)
 
-#Planet
+# Planet
+
 
 @app.route("/planet/<int:id>")
 def planet(id):
     sql = """
         SELECT * FROM Planets
         WHERE ID = ?;"""
-    planet = query_db(sql,(id,),True)
+    planet = query_db(sql, (id,), True)
     sql = """
-        SELECT MoonID, Name, ImageURL FROM Moons 
+        SELECT MoonID, Name, ImageURL FROM Moons
         WHERE PlanetID = ?;"""
-    moons = query_db(sql,(id,),False)
+    moons = query_db(sql, (id,), False)
     return render_template("planet.html", planet=planet, moons=moons)
 
-#Moon
+# Moon
+
 
 @app.route("/moon/<int:id>")
 def moon(id):
     sql = """
         SELECT * FROM Moons
         WHERE MoonID = ?;"""
-    moon = query_db(sql,(id,),True)
+    moon = query_db(sql, (id,), True)
     return render_template("moon.html", moon=moon)
 
-############################# Runner ###################################################################
+# Runner ##########################################
+
+
 if __name__ == "__main__":
     app.run(debug=True)
